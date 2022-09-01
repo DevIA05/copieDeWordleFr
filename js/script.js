04f48822c7d939cfc4ec7abe6de4599c91438b76
DevIA.E05 <DevIA.E05@afpar.com>
2022-09-01 12:02:01 +0400

    ajout: parcours des colonnes (ajout d'une lettre + suppression d'une lettre) + ajout d'une lettre au clavier virtuelet réelle

--- js/script.js
+++ js/script.js
@@ -1,23 +1,61 @@
-// onClickTouch
-var touch; 
+// Variable ================================================
+let line = 1; 
+let column = 1; 
+let writtenWord = ""; 
+const mot = "boule"; 
+
+
+// Listen ==================================================
+// Listen the button pressed
 document.querySelectorAll('.keyboard .touch').forEach(item => {
-    item.addEventListener('click', event => {
-        touch = event.target.textContent;
-        console.log(touch)
+    item.addEventListener('click', e => {
+        writeLetterinCase(line, column, e.target.textContent)
+        goToCol();
+        main(line, column);
     })
 })
 
-// Start
-var ligne = 1;
-var colonne = 1;
+// Listen
+document.addEventListener('keydown', (e) => {
+    console.log("key : "+e.key +" ---- "+ "code :"+ e.code);
+    if(e.key !== null){
+        if((e.code === "Backspace" || e.code === "Delete") && column>1){
+            unLightCase(line,column);
+            goToCol(-1);
+            writeLetterinCase(line, column, "")
+            highlighCase(line, column)
+        } 
+        if(!(/[a-z]/.test(e.key) && e.key.length<=1)) e.key = "";
+        else {
+            writeLetterinCase(line, column, e.key)
+            unLightCase(line,column)
+            goToCol(1);
+            highlighCase(line, column)
 
-function enableInput(l, c){ 
-    document.getElementById(l).children[c].disabled=false; 
+        }
+    }
+  }, false);
+
+// Function ================================================
+
+// Go to the adjacent column
+function goToCol(i){if(column < 5) column += i;}
+
+// Go to the next line
+function upline(){if(line<6) line+=1}
+
+// Write letter in the current case
+function writeLetterinCase(l, c, val){ 
+    document.getElementById(l.toString()+c.toString()).innerHTML = val;
+    writtenWord += val;
+    // console.log(writtenWord)
 }
 
-enableInput("1","1")
+// Highligh the current case
+function highlighCase(l,c){ document.getElementById(l.toString()+c.toString()).style.borderColor="#73adff";}
 
-function highlighCase(){ 
-    document.getElementById(l+c).disabled=false; 
-    
-}
+// Unligh the previous case
+function unLightCase(l,c){ document.getElementById(l.toString()+(c).toString()).style.borderColor="#646464";}
+
+// Trigger function ================================================
+highlighCase(line, column)
