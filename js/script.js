10b9b70055a24eb8096b9ef9c2d7339280641d28
DevIA.E05 <DevIA.E05@afpar.com>
2022-09-01 13:35:46 +0400

    ajout: suppression d'une lettre à partir du clavier virtuel

--- js/script.js
+++ js/script.js
@@ -9,30 +9,24 @@ const mot = "boule";
 // Listen the button pressed
 document.querySelectorAll('.keyboard .touch').forEach(item => {
     item.addEventListener('click', e => {
-        writeLetterinCase(line, column, e.target.textContent)
-        goToCol();
-        main(line, column);
+        switch(e.target.id){
+            case "enter":
+                break;
+            case "suppr":
+                goToPreviousCase();
+                break;
+            default:
+                goToNextCase(e.target.innerHTML);
+        }
     })
 })
 
 // Listen
 document.addEventListener('keydown', (e) => {
-    console.log("key : "+e.key +" ---- "+ "code :"+ e.code);
-    if(e.key !== null){
-        if((e.code === "Backspace" || e.code === "Delete") && column>1){
-            unLightCase(line,column);
-            goToCol(-1);
-            writeLetterinCase(line, column, "")
-            highlighCase(line, column)
-        } 
+      if(e.key !== null){
+        if((e.code === "Backspace" || e.code === "Delete") && column>1) goToPreviousCase(); 
         if(!(/[a-z]/.test(e.key) && e.key.length<=1)) e.key = "";
-        else {
-            writeLetterinCase(line, column, e.key)
-            unLightCase(line,column)
-            goToCol(1);
-            highlighCase(line, column)
-
-        }
+        else goToNextCase(e.key) 
     }
   }, false);
 
@@ -45,17 +39,28 @@ function goToCol(i){if(column < 5) column += i;}
 function upline(){if(line<6) line+=1}
 
 // Write letter in the current case
-function writeLetterinCase(l, c, val){ 
-    document.getElementById(l.toString()+c.toString()).innerHTML = val;
-    writtenWord += val;
-    // console.log(writtenWord)
-}
+function writeLetterinCase(l, c, val){ document.getElementById(l.toString()+c.toString()).innerHTML = val; }
 
 // Highligh the current case
-function highlighCase(l,c){ document.getElementById(l.toString()+c.toString()).style.borderColor="#73adff";}
+function highlighCase(l,c){ document.getElementById(l.toString()+c.toString()).style.borderColor="#73adff"; }
 
 // Unligh the previous case
-function unLightCase(l,c){ document.getElementById(l.toString()+(c).toString()).style.borderColor="#646464";}
+function unLightCase(l,c){ document.getElementById(l.toString()+(c).toString()).style.borderColor="#646464"; }
+
+function goToPreviousCase(){
+    unLightCase(line,column);
+    goToCol(-1);
+    writeLetterinCase(line, column, "")
+    highlighCase(line, column)
+}
+
+function goToNextCase(str){
+    writeLetterinCase(line, column, str)
+    unLightCase(line,column)
+    goToCol(1);
+    highlighCase(line, column)
+}
+
 
 // Trigger function ================================================
 highlighCase(line, column)
